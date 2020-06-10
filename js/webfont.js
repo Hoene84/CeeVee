253c0a75b98230f168011e10ff28353414c339c3
Simon Honegger <simon.honegger@gmail.com>
2020-06-10 16:40:40 +0200

    style: use google Raleway font

--- js/webfont.js
+++ js/webfont.js
@@ -1,6 +1,6 @@
 
 WebFont.load({
     google: {
-        families: ['Share Tech Mono:100,200,300,400,500,600,700,800,900,100italic,200italic,300italic,400italic,500italic,600italic,700italic,800italic,900italic','Source Sans Pro:100,200,300,400,500,600,700,800,900,100italic,200italic,300italic,400italic,500italic,600italic,700italic,800italic,900italic','Rubik:100,200,300,400,500,600,700,800,900,100italic,200italic,300italic,400italic,500italic,600italic,700italic,800italic,900italic:latin,latin-ext,cyrillic-ext,greek-ext,greek,vietnamese,cyrillic']
+        families: ['Raleway']
     }
 });
